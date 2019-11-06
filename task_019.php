ce1cb0dcad90ba251425fc3bdf6ebe6434f90454
Ivan Mironov <ivail89@mail.ru>
2019-11-06 19:58:26 +0300

    Add index

--- task_019.php
+++ task_019.php
@@ -20,13 +20,12 @@ $arr_word = array_flip(str_split($word)); // Получаем массив с и
 foreach ($arr_word as $i => $value) { $arr_word[$i] = []; } // В эти массивы будем записывать найденные буквы
 $width = count($matrix[0]);
 $height = count($matrix);
-$letter_used = array_fill(0, $height, array_fill(0, $width, false)); // true - если это символ уже добавлен
 
 // Определяем все варианты начала слова
 foreach ($matrix as $i => $string){
 	foreach ($string as $j => $letter){
 		if ($letter == $word[0]) {
-      $arr_word[$word[0]][] = [$j, $i];
+      $arr_word[$word[0]][] = [$j, $i, uniqid(), null];
     }
   }
 }
@@ -36,33 +35,33 @@ $len_word = strlen($word) - 1;
 foreach ($letters as $key => $letter){
   if ($key == $len_word) continue;
   foreach ($arr_word[$letter] as $k => $pos){
-    $res = selectionSuitable($pos[1], $pos[0], $letters[$key+1]);
-    if (empty($res)) unset ($arr_word[$letter][$k]);
+    $res = selectionSuitable($pos[2], $pos[1], $pos[0], $letters[$key+1]);
+    if (empty($res)){
+      unset ($arr_word[$letter][$k]);
+      $letter_used[$pos[1]][$pos[0]] = false;
+    }
     foreach ($res as $r)
-      if (!$letter_used[$r[1]][$r[0]]){
-        $arr_word[$letters[$key+1]][] = $r;
-        $letter_used[$r[1]][$r[0]] = true;
-      }
+      $arr_word[$letters[$key+1]][] = $r;
   }
 }
 
-
 /*
-* i, j - кординаты буквы вокруг которой проверяем следующую букву
-* next_letter - искомая буква
+ * i, j - кординаты буквы вокруг которой проверяем следующую букву
+ * next_letter - искомая буква
+ * prev_id - ИД предыдущей буквы
 */
-function selectionSuitable($i, $j, $next_letter){
+function selectionSuitable($prev_id, $i, $j, $next_letter){
   $res = [];
   global $matrix, $width, $height;
 
-  if (($j != 0) && ($matrix[$i][$j-1] == $next_letter)) $res[] = [$i,$j-1]; //Направление 0 1
-  if (($i+1 != $width) && ($j != 0) && ($matrix[$i+1][$j-1] == $next_letter)) $res[] = [$i+1,$j-1]; //Направление 1 1
-  if (($i+1 != $width) && ($matrix[$i+1][$j] == $next_letter)) $res[] = [$i+1,$j]; //Направление 1 0
-  if (($i+1 != $width) && ($j+1 != $height) && ($matrix[$i+1][$j+1] == $next_letter)) $res[] = [$i+1,$j+1]; //Направление 1 -1
-  if (($j+1 != $height) && ($matrix[$i][$j+1] == $next_letter)) $res[] = [$i,$j+1]; //Направление 0 -1
-  if (($i != 0) && ($j+1 != $height) && ($matrix[$i-1][$j+1] == $next_letter)) $res[] = [$i-1,$j+1]; //Направление -1 -1
-  if (($i != 0) && ($matrix[$i-1][$j] == $next_letter)) $res[] = [$i-1,$j]; //Направление -1 0
-  if (($i != 0) && ($j != 0) && ($matrix[$i-1][$j-1] == $next_letter)) $res[] = [$i-1,$j-1]; //Направление -1 1
+  if (($j != 0) && ($matrix[$i][$j-1] == $next_letter)) $res[] = [$i,$j-1, uniqid(), $prev_id]; //Направление 0 1
+  if (($i+1 != $width) && ($j != 0) && ($matrix[$i+1][$j-1] == $next_letter)) $res[] = [$i+1, $j-1, uniqid(), $prev_id]; //Направление 1 1
+  if (($i+1 != $width) && ($matrix[$i+1][$j] == $next_letter)) $res[] = [$i+1, $j, uniqid(), $prev_id]; //Направление 1 0
+  if (($i+1 != $width) && ($j+1 != $height) && ($matrix[$i+1][$j+1] == $next_letter)) $res[] = [$i+1, $j+1, uniqid(), $prev_id]; //Направление 1 -1
+  if (($j+1 != $height) && ($matrix[$i][$j+1] == $next_letter)) $res[] = [$i, $j+1, uniqid(), $prev_id]; //Направление 0 -1
+  if (($i != 0) && ($j+1 != $height) && ($matrix[$i-1][$j+1] == $next_letter)) $res[] = [$i-1,$j+1, uniqid(), $prev_id]; //Направление -1 -1
+  if (($i != 0) && ($matrix[$i-1][$j] == $next_letter)) $res[] = [$i-1, $j, uniqid(), $prev_id]; //Направление -1 0
+  if (($i != 0) && ($j != 0) && ($matrix[$i-1][$j-1] == $next_letter)) $res[] = [$i-1, $j-1, uniqid(), $prev_id]; //Направление -1 1
 
   return $res;
 }
