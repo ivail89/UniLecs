04df38e4a715ccdfcca7c552eabe6e8b2fdb2d1d
Ivan Mironov <ivail89@mail.ru>
2020-04-02 12:11:38 +0300

    task_061 - solved

--- functions.php
+++ functions.php
@@ -45,7 +45,7 @@ function distanceBetweenPoints($x1, $y1, $x2, $y2){
   return sqrt(($x2 - $x1)**2 + ($y2 - $y1)**2);
 }
 
-echo distanceBetweenPoints(0, 0, 10, 10);
+//echo distanceBetweenPoints(0, 0, 10, 10);
 //$r = combinationsM_N(4, 3);
 //printMatrix($r);
 
