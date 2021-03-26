0529e6d7961152e1280124a2045cb8a9eaa3dccb
Ivan Mironov <i.mironov@roseltorg.ru>
2021-03-26 14:05:07 +0300

    task_184 - solved

--- functions.php
+++ functions.php
@@ -364,3 +364,14 @@ function getMultipliers(int $n)
   }
   return $res;
 }
+
+// Вычисление N-го числа последовательности Фибоначи
+function fibonachi(int $n)
+{
+  $arr = [1 => 1, 2 => 1];
+  $n++;
+  for ($i = 3; $i < $n; $i++) {
+    $arr [$i] = $arr[$i-1] + $arr[$i-2];
+  }
+  return $arr[$n-1];
+}
